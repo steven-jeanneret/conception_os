06a7c9f3397a8c3c988bfab337856ac7ed9711b4
Steven Jeanneret <steven.jeanneret@outlook.com>
2019-02-18 15:11:10 +0100

    clic show arrow

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,35 @@
 <template>
-  <div id="app">
-    <div class="row">
-      <div class="col-6">
-    <Table :values="[3,4,5]"/>
-    <Table :values="[7,8,9]"/>
+    <div id="app">
+        <div class="row">
+            <div class="col-6">
+                <Table :v1="[3,4,5]" :v2="[7,8,9]"/>
+            </div>
+            <div class="col-6">
+                <Table :v1="[1,6,8]" :v2="[6,4,1]"/>
+            </div>
         </div>
-      <div class="col-6">
-    <Table :values="[1,6,8]"/>
-    <Table :values="[6,4,1]"/>
-      </div>
-      </div>
 
-  </div>
+    </div>
 </template>
 
 <script>
-import Table from './components/Table'
+  import Table from './components/Table'
 
-export default {
-  name: 'app',
-  components: {
-    Table
+  export default {
+    name: 'app',
+    components: {
+      Table
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,11 +1,29 @@
 <template>
-    <div class="my-2 table_container border border-primary" :style="{width: sizeT}">
-        <cell v-for="value in values" :value="value"/>
+    <div>
+        <div class="col-6">
+            <div class="my-2 table_container border border-primary" :style="{width: sizeT}">
+                <cell v-for="value in v1" :value="value"/>
+            </div>
+        </div>
+        <div class="col-6">
+            <div class="my-2" :style="{width: sizeT}">
+                <i class="fas fa-arrow-down" v-for="i in slice(v1.length)" v-show="show[i]" style="width: 140px"></i>
+            </div>
+        </div>
+        <div class="col-6">
+            <div class="my-2 table_container border border-primary" :style="{width: sizeT}">
+                <cell v-for="value in v2" :value="value"/>
+            </div>
+        </div>
+        <div class="col-6">
+            <button @click="startAnimation">Start</button>
+        </div>
     </div>
 </template>
 
 <script>
   import Cell from './Cell'
+  import Vue from 'vue';
 
   export default {
     name: 'Table',
@@ -13,18 +31,35 @@
     data () {
       return {
         sizeTable: 0,
+        animRunning: false,
+        show: [],
+      }
+    },
+    methods: {
+      startAnimation () {
+        //Vue.set(this.show, 0, true)
+        Vue.set(this.show, 0, true)
+        Vue.set(this.show, 2, true)
+        //this.animRunning = true
+      },
+      slice (n) {
+        return Array.from(new Array(n), (val, index)=>index)
       }
     },
     props: {
-      values: { type: Array },
+      v1: { type: Array },
+      v2: { type: Array },
     },
     computed: {
       sizeT () {
         return this.sizeTable + 'px'
-      }
+      },
     },
     mounted () {
-      this.sizeTable = this.values.length * 140
+      this.sizeTable = this.v1.length * 140
+      for (let i = 0; i < this.v1.length; i++) {
+        this.show.push(false)
+      }
     },
   }
 </script>
